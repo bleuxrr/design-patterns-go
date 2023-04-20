1177925b11411f34dcde5ee029937a5ce3c39998
bleuxxr <bleuxrr@gmail.com>
2023-04-21 03:48:24 +0800

    add null object

---
+++ nullObject/agency.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	college1 := createCollege1()
+	college2 := createCollege2()
+	totalProfessors := 0
+	departmentArray := []string{"computerscience", "mechanical", "civil", "electronics"}
+
+	for _, departmentName := range departmentArray {
+		d := college1.getDepartment(departmentName)
+		totalProfessors += d.getNumberOfProfessors()
+	}
+	fmt.Printf("total number of professors in college1 is %d\n", totalProfessors)
+
+	totalProfessors = 0
+	for _, departmentName := range departmentArray {
+		d := college2.getDepartment(departmentName)
+		totalProfessors += d.getNumberOfProfessors()
+	}
+	fmt.Printf("total number of professors in college2 is %d\n", totalProfessors)
+}
+
+func createCollege1() *college {
+	college := &college{}
+	college.addDepartment("computerscience", 4)
+	college.addDepartment("mechanical", 5)
+	return college
+}
+
+func createCollege2() *college {
+	college := &college{}
+	college.addDepartment("computerscience", 2)
+	return college
+}
