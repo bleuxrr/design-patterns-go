adf258b3904f71e11226b31a7eff1317d805a9c9
bleuxxr <bleuxrr@gmail.com>
2023-04-22 04:12:19 +0800

    add visitor

--- visitor/areaCalculator.go
+++ visitor/areaCalculator.go
@@ -7,13 +7,13 @@ type areaCalculator struct {
 }
 
 func (a *areaCalculator) visitForSquare(s *square) {
-	fmt.Println("calculating area for square")
+	fmt.Println("calculating area for square ", s.side*s.side)
 }
 
-func (a *areaCalculator) visitForCircle(s *circle) {
-	fmt.Println("calculating area for circle")
+func (a *areaCalculator) visitForCircle(c *circle) {
+	fmt.Println("calculating area for circle ", 3.14*float64(c.radius*c.radius))
 }
 
-func (a *areaCalculator) visitForRectangle(s *rectangle) {
-	fmt.Println("calculating area for rectangle")
+func (a *areaCalculator) visitForRectangle(r *rectangle) {
+	fmt.Println("calculating area for rectangle ", r.l*r.b)
 }
