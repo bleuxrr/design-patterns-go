adb6dc7a4cb39cc6b72a7fba877f4596c9bf5366
bleuxxr <bleuxrr@gmail.com>
2023-04-21 22:51:35 +0800

    add strategy

---
+++ strategy/cache.go
@@ -0,0 +1,34 @@
+package main
+
+type cache struct {
+	storage      map[string]string
+	evictionAlgo evictionAlgo
+	capacity     int
+	maxCapacity  int
+}
+
+func initCache(e evictionAlgo) *cache {
+	return &cache{
+		storage:      map[string]string{},
+		evictionAlgo: e,
+		capacity:     0,
+		maxCapacity:  2,
+	}
+}
+
+func (c *cache) setEvictionAlgo(e evictionAlgo) {
+	c.evictionAlgo = e
+}
+
+func (c *cache) add(key, value string) {
+	if c.capacity == c.maxCapacity {
+		c.evict()
+	}
+	c.capacity++
+	c.storage[key] = value
+}
+
+func (c *cache) evict() {
+	c.evictionAlgo.evict(c)
+	c.capacity--
+}
